4485705109a2467aba86efad3d9002628de7f971
JiaruiElijah <jz5452@nyu.edu>
2022-10-24 14:45:38 -0400

    A
    D:\midterm_jiarui

---
+++ empty-example - 副本/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+
+    createCanvas(400, 400);
+    background(0,0,255);
+}
+
+function draw() {
+  // put drawing code here
+
+rect(200,200,100,100);
+}
